1c36606e19e575a29a8b7635f945dc7c1f680599
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2023-12-12 10:55:59 +0100

    update: q2

--- code/q2.c
+++ code/q2.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <webots/camera.h>
 #include <webots/distance_sensor.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
 
-#define SENSOR_COUNT 4
+#define SENSOR_NUMBER 9
 
-static WbDeviceTag front_sensors[SENSOR_COUNT];
-static WbDeviceTag left_motor, right_motor;
+#define FRONT_SENSOR_COUNT 4
+#define RANGE 2000
+#define BOUND(x, a, b) (((x) < (a)) ? (a) : ((x) > (b)) ? (b) : (x))
 
+static WbDeviceTag sensors[SENSOR_NUMBER], left_motor, right_motor;
+
+static const double matrix[9][2] = {
+    {-2.67, -2.67},  {-10.86, 21.37}, {-16.03, 26.71},
+    {-37.4, 37.4},   {37.4, -32.06},  {26.71, -21.37},
+    {21.37, -10.86}, {-2.67, -2.67},  {-5.34, -5.34}};
+
+static const int num_sensors = SENSOR_NUMBER;
+static const double range = RANGE;
 static int time_step = 0;
-static const double speed = 10;
+static const double max_speed = 19.1;
+
+void enable_sensors() {
+  char sensors_name[5];
+  sprintf(sensors_name, "%s", "ds0");
+  int i;
+
+  for (i = 0; i < num_sensors; i++) {
+    sensors[i] = wb_robot_get_device(sensors_name);
+    wb_distance_sensor_enable(sensors[i], time_step);
+
+    if ((i + 1) >= 10) {
+      sensors_name[2] = '1';
+      sensors_name[3]++;
+
+      if ((i + 1) == 10) {
+        sensors_name[3] = '0';
+        sensors_name[4] = '\0';
+      }
+    } else {
+      sensors_name[2]++;
+    }
+  }
+}
 
 static void initialize() {
   wb_robot_init();
 
   time_step = wb_robot_get_basic_time_step();
 
-  char sensors_name[4];
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    sprintf(sensors_name, "ds%d", i+2);
-    front_sensors[i] = wb_robot_get_device(sensors_name);
-    wb_distance_sensor_enable(front_sensors[i], time_step);
-  }
+  enable_sensors();
 
   left_motor = wb_robot_get_device("left wheel motor");
   right_motor = wb_robot_get_device("right wheel motor");
@@ -31,15 +60,46 @@ static void initialize() {
   wb_motor_set_position(right_motor, INFINITY);
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
+
+  const char *robot_name = wb_robot_get_name();
+  printf("The %s robot is initialized, it uses %d distance sensors\n",
+         robot_name, num_sensors);
 }
 
-void process() {
+void braitenberg() {
+  while (wb_robot_step(time_step) != -1) { // Run simulation
+    int i, j;
+    double speed[2];
+    double sensors_value[num_sensors];
+
+    for (i = 0; i < num_sensors; i++)
+      sensors_value[i] = wb_distance_sensor_get_value(sensors[i]);
+
+    for (i = 0; i < 2; i++) {
+      speed[i] = 0.0;
+
+      for (j = 0; j < num_sensors; j++) {
+        speed[i] += matrix[j][i] * (1.0 - (sensors_value[j] / range));
+      }
+
+      speed[i] = BOUND(speed[i], -max_speed, max_speed);
+    }
+
+    wb_motor_set_velocity(left_motor, speed[0]);
+    wb_motor_set_velocity(right_motor, speed[1]);
+  }
+}
+
+void question_2() {
+  static const int front_sensor_indexes[FRONT_SENSOR_COUNT] = { 2, 3, 4, 5 };
+  static const double speed = 10;
+  
   while (wb_robot_step(time_step) != -1) {
-    double sensors_value[SENSOR_COUNT];
+    double sensors_value[FRONT_SENSOR_COUNT];
     double current_speed = speed;
 
-    for (int i = 0; i < SENSOR_COUNT; i++) {
-      sensors_value[i] = wb_distance_sensor_get_value(front_sensors[i]);
+    for (int i = 0; i < FRONT_SENSOR_COUNT; i++) {
+      sensors_value[i] = wb_distance_sensor_get_value(sensors[front_sensor_indexes[i]]);
       if (sensors_value[i] > 100.0 )
       {
         current_speed = 0;
@@ -52,10 +112,9 @@ void process() {
   }
 }
 
-
 int main() {
   initialize();
-  process();
-  wb_robot_cleanup();
+  question_2();
+
   return 0;
 }
